51044ff7329dc94a1906bec11b32412f3e8c8c2e
whitescan <jeongseop@whitescan.com>
2026-05-21 10:08:18 +0900

    chore : asnyc 안정화

--- src/main/resources/db/manual/20260521_review_job_stability.sql
+++
@@ -1,5 +0,0 @@
--- P7 review job stability migration.
--- This project does not currently run Flyway/Liquibase. Apply this manually before deploying the P7 backend.
-
-alter table pull_request
-    add column if not exists review_run_id varchar(36);
